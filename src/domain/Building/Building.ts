d255b7d6982a0f4a8b7d581d05151f015c6718a3
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-18 16:48:54 +0100

    Added Domain Model classes relative to Building Aggregate

--- src/domain/Building/Building.ts
+++ src/domain/Building/Building.ts
@@ -1,14 +1,18 @@
-import { AggregateRoot } from '../core/domain/AggregateRoot';
+import { AggregateRoot } from '../../core/domain/AggregateRoot';
+import { UniqueEntityID } from "../../core/domain/UniqueEntityID";
 import { BuildingDescription } from './BuildingDescription';
 import { BuildingName } from './BuildingName';
 
-interface BuildingProps {
-  buildingDescription: BuildingDescription;
-  buildingName: BuildingName;
+  interface BuildingProps {
+    buildingDescription: BuildingDescription;
+    buildingName: BuildingName;
+  }
 
+  export class Building extends AggregateRoot<BuildingProps> {
 
-}
+    
+    private constructor (props: BuildingProps, id?: UniqueEntityID) {
+      super(props, id);
+    }
 
-export class Building extends AggregateRoot<BuildingProps> {
-
-}
+  }
--- src/domain/Building/BuildingDescription.ts
+++ src/domain/Building/BuildingDescription.ts
@@ -1,1 +1,14 @@
+import { ValueObject } from '../../core/domain/ValueObject';
 
+  
+  interface BuildingDescriptionProps {
+    value: string;
+  }
+
+  export class BuildingDescription extends ValueObject<BuildingDescriptionProps> {
+
+    private constructor (props : BuildingDescriptionProps){
+      super(props);
+    }
+    
+  }
