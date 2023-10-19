8f6c512f3cc048bbafc6b696fe8d0280c91c2770
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-19 08:35:33 +0100

    Updated Domain classes Building added list of Floor

--- src/domain/Building/Building.ts
+++ src/domain/Building/Building.ts
@@ -2,10 +2,12 @@ import { AggregateRoot } from '../../core/domain/AggregateRoot';
 import { UniqueEntityID } from "../../core/domain/UniqueEntityID";
 import { BuildingDescription } from './BuildingDescription';
 import { BuildingName } from './BuildingName';
+import { Floor } from '../Floor/Floor';
 
   interface BuildingProps {
     buildingDescription: BuildingDescription;
     buildingName: BuildingName;
+    floors: Floor[];
   }
 
   export class Building extends AggregateRoot<BuildingProps> {
