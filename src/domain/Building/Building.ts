e6723deb217691e6113ac457bd0ca6473bba0670
jose <1211089@isep.ipp.pt>
2023-10-21 16:19:21 +0100

    US 150 created create method in Building.ts

--- src/domain/Building/Building.ts
+++ src/domain/Building/Building.ts
@@ -3,11 +3,13 @@ import { UniqueEntityID } from "../../core/domain/UniqueEntityID";
 import { BuildingDescription } from './BuildingDescription';
 import { BuildingName } from './BuildingName';
 import { Floor } from '../Floor/Floor';
+import { IBuildingDTO } from '../../dto/building/IBuildingDTO';
+import { Result } from '../../core/logic/Result';
 
 interface BuildingProps {
   buildingName: BuildingName
   buildingDescription: BuildingDescription;
-  floors: Floor[];
+  //floors: Floor[];
 }
 
 export class Building extends AggregateRoot<BuildingProps> {
@@ -24,4 +26,20 @@ export class Building extends AggregateRoot<BuildingProps> {
     return this.props.buildingDescription
   }
 
+
+  public static create(buildingDto: IBuildingDTO, id: UniqueEntityID): Result<Building> {
+    const name = buildingDto.buildingName
+    const description = buildingDto.buildingDescription
+
+    if (!!name === false || !!description === false || name.length === 0 || description.length === 0) {
+      return Result.fail<Building>('Missing paramethers')
+    }
+
+    const building = new Building({
+      buildingName: new BuildingName(name),
+      buildingDescription: new BuildingDescription({value: description})
+    }, id)
+
+    return Result.ok<Building>(building)
+  }
 }
