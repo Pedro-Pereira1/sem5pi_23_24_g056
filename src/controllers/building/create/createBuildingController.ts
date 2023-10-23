c9b8247d36f8e8f7767f9229c5982525038c722e
Tiago <1211128@isep.ipp.pt>
2023-10-23 15:32:42 +0100

    Implemented the method createBuilding in the createBuildingController class, regarding US150

--- src/controllers/building/create/createBuildingController.ts
+++ src/controllers/building/create/createBuildingController.ts
@@ -1,8 +1,11 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import ICreateBuildingController from "../../IControllers/building/createBuilding/ICreateBuildingController";
 import { Inject, Service } from "typedi";
 import config from "../../../../config";
 import ICreateBuildingService from "../../../services/IServices/building/ICreateBuildingService";
+import { BuildingMap } from "../../../mappers/building/BuildingMap";
+import { IBuildingDTO } from "../../../dto/building/IBuildingDTO";
+import { Result } from "../../../core/logic/Result";
 
 @Service()
 export default class CreateBuildingController implements ICreateBuildingController {
@@ -12,7 +15,19 @@ export default class CreateBuildingController implements ICreateBuildingControll
     ) 
     {}
 
-    createBuilding(req: Request, res: Response) {
+    public async createBuilding(req: Request, res: Response, next: NextFunction) {
+        try {
+            const buildingOrError = await this.service.createBuilding(req.body as IBuildingDTO) as Result<IBuildingDTO>
+        
+            if (buildingOrError.isFailure) {
+                return res.status(402).send
+            }
+        
+            const buildingDTO = buildingOrError.getValue();
+            return res.json(buildingDTO).status(201);
 
+        }catch (e){
+            return next(e);
+        }
     }
 }
