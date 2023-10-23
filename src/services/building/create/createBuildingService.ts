1258835d258a6997bd445c7ab273dd324dc31153
Tiago <1211128@isep.ipp.pt>
2023-10-23 15:41:27 +0100

    Implemented the method createBuilding in the createBuildingService class, regarding US150

--- src/services/building/create/createBuildingService.ts
+++ src/services/building/create/createBuildingService.ts
@@ -4,17 +4,33 @@ import { IBuildingDTO } from "../../../dto/building/IBuildingDTO";
 import ICreateBuildingService from "../../IServices/building/ICreateBuildingService";
 import config from "../../../../config";
 import IBuildingRepo from "../../IRepos/building/IBuildingRepo";
+import { Building } from "../../../domain/Building/Building";
+import { BuildingMap } from "../../../mappers/building/BuildingMap";
 
 @Service()
 export default class CreateBuildingService implements ICreateBuildingService {
 
     constructor(
         @Inject(config.repos.building.name) private buildingRepo: IBuildingRepo
-    )
-    {}
+    ) { }
 
-    createBuilding(BuildingDto: IBuildingDTO): Promise<Result<IBuildingDTO>> {
-        throw new Error("Method not implemented.");
+    public async createBuilding(buildingDto: IBuildingDTO): Promise<Result<IBuildingDTO>> {
+        try{
+            const buildingOrError = await Building.create(buildingDto)
+
+            if (buildingOrError.isFailure) {
+                return Result.fail<IBuildingDTO>(buildingOrError.errorValue())
+            }
+
+            const buildingResult = buildingOrError.getValue()
+
+            await this.buildingRepo.save(buildingResult);
+
+            const buildingDtoResult = BuildingMap.toDto(buildingResult) as IBuildingDTO
+            return Result.ok<IBuildingDTO>(buildingDtoResult)
+
+        } catch(e) {
+            throw e
+        }
     }
-
 }
