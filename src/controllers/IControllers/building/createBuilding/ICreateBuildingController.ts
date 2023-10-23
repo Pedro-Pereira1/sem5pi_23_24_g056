f796180dc9edc8f8366307731bcaac16d7ad297c
Tiago <1211128@isep.ipp.pt>
2023-10-23 15:31:17 +0100

    Added next function parameter to createBuilding method in ICreateBuildingController, regarding US150

--- src/controllers/IControllers/building/createBuilding/ICreateBuildingController.ts
+++ src/controllers/IControllers/building/createBuilding/ICreateBuildingController.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export default interface ICreateBuildingController{
-    createBuilding(req: Request, res: Response)
+    createBuilding(req: Request, res: Response, next: NextFunction)
 }
