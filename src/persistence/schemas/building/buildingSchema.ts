b2d563850326282cb24290e692da14af938c3a21
Tiago <1211128@isep.ipp.pt>
2023-10-23 15:40:50 +0100

    Completed the data schema with new necessary parameter, regarding US150

--- src/persistence/schemas/building/buildingSchema.ts
+++ src/persistence/schemas/building/buildingSchema.ts
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 import { IBuildingPersistence } from "../../../dataschema/building/IBuildingPersistence";
+import { BuildingSize } from "../../../domain/Building/BuildingSize";
 
 const BuildingSchema = new mongoose.Schema(
     {
         buildingDescription: {type: String},
-        buildingName: {type: String, unique: true}
+        buildingName: {type: String},
+        buildingCode: {type: String, unique: true},
+        buildingLength: {type: Number},
+        buildingWidth: {type: Number}
     },
     {
         timestamps: true
