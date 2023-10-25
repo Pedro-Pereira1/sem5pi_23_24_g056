f563e31210c895271850e8f8f568a464c5919033
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-25 19:44:23 +0100

    Updated Controller class to List Buildings for US180

--- src/controllers/building/list/listBuildingsMaxMinFloorsController.ts
+++ src/controllers/building/list/listBuildingsMaxMinFloorsController.ts
@@ -1,5 +1,5 @@
 import { Inject, Service } from "typedi";
-import IListBuildingsMaxMinFloorsController from "../../IControllers/building/list/IListBuildingsMaxMinFloorsService";
+import IListBuildingsMaxMinFloorsController from "../../IControllers/building/list/IListBuildingsMaxMinFloorsController";
 import { Request, Response, NextFunction } from "express";
 import { ParamsDictionary } from "express-serve-static-core";
 import config from "../../../../config";
