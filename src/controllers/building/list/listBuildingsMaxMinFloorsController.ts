9f7b88a879e5255c8eb4174aaf366919a61a9042
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-25 19:42:10 +0100

    Added Controller class to List Buildings for US180

---
+++ src/controllers/building/list/listBuildingsMaxMinFloorsController.ts
@@ -0,0 +1,27 @@
+import { Inject, Service } from "typedi";
+import IListBuildingsMaxMinFloorsController from "../../IControllers/building/list/IListBuildingsMaxMinFloorsService";
+import { Request, Response, NextFunction } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import config from "../../../../config";
+import IListBuildingsMaxMinFloorsService from "../../../services/IServices/building/IListBuildingsMaxMinFloorsService";
+
+@Service()
+export default class ListBuildingsMaxMinFloorsController implements IListBuildingsMaxMinFloorsController {
+
+    constructor(
+        @Inject(config.services.listBuildingsMaxMinFloors.name) private listAllBuildingsService: IListBuildingsMaxMinFloorsService
+    )
+    {}
+
+    listBuildingsMaxMinFloors(req: Request, res: Response, next: NextFunction) {
+        try{
+            const buildings = this.listAllBuildingsService.listBuildingsMaxMinFloors
+            res.status(200).json(buildings);
+        }catch(error){
+            console.error("Error in listBuildingsMaxMinFloors:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+
+
+}
