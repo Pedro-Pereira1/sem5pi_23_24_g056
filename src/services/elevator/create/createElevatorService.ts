df650df7f94f8f7caa1324e598c667af6137d7f0
Tiago <1211128@isep.ipp.pt>
2023-10-26 22:12:17 +0100

    Added missing parameter to create an elevator, regarding US270

--- src/services/elevator/create/createElevatorService.ts
+++ src/services/elevator/create/createElevatorService.ts
@@ -6,6 +6,13 @@ import { Elevator } from "../../../domain/Elevator/Elevator";
 import ElevatorMap from "../../../mappers/elevator/ElevatorMap";
 import ICreateElevatorService from "../../IServices/elevator/ICreateElevatorService";
 import IElevatorDTO from "../../../dto/elevator/IElevatorDTO";
+import { ElevatorCoordinateX } from "../../../domain/Elevator/ElevatorCoordinateX";
+import { ElevatorCoordinateY } from "../../../domain/Elevator/ElevatorCoordinateY";
+import { ElevatorIdentificationNumber } from "../../../domain/Elevator/ElevatorIdentificationNumber";
+import { ElevatorBrand } from "../../../domain/Elevator/ElevatorBrand";
+import { ElevatorSerialNumber } from "../../../domain/Elevator/ElevatorSerialNumber";
+import { ElevatorModel } from "../../../domain/Elevator/ElevatorModel";
+import { ElevatorDescription } from "../../../domain/Elevator/ElevatorDescription";
 
 
 @Service()
@@ -18,9 +25,18 @@ export default class CreateElevatorService implements ICreateElevatorService {
         throw new Error("Method not implemented.");
     }
 
-    public async createElevator(ElevatorDto: IElevatorDTO): Promise<Result<IElevatorDTO>> {
+    public async createElevator(elevatorDto: IElevatorDTO): Promise<Result<IElevatorDTO>> {
         try{
-            const ElevatorOrError = await Elevator.create(ElevatorDto)
+            const ElevatorOrError = await Elevator.create(
+                {
+                    elevatorCoordinateX: new ElevatorCoordinateX({elevatorCoordinateX: elevatorDto.elevatorCoordinateX}),
+                    elevatorCoordinateY: new ElevatorCoordinateY({elevatorCoordinateY: elevatorDto.elevatorCoordinateY}),
+                    elevatorIdentificationNumber: new ElevatorIdentificationNumber({identificationNumber: elevatorDto.elevatorIdentificationNumber}),
+                    elevatorBrand: new ElevatorBrand({brand: elevatorDto.elevatorBrand}),
+                    elevatorDescription: new ElevatorDescription({description: elevatorDto.elevatorDescription}),
+                    elevatorModel: new ElevatorModel({model: elevatorDto.elevatorModel}),
+                    elevatorSerialNumber: new ElevatorSerialNumber({serialNumber: elevatorDto.elevatorSerialNumber})
+                }, )
 
             if (ElevatorOrError.isFailure) {
                 return Result.fail<IElevatorDTO>(ElevatorOrError.errorValue())
