5b23a49f8fc85eb78afb9f9174ca1e8a4c445994
Alexandre Geração <1211151@isep.ipp.pt>
2023-10-26 22:23:36 +0100

    Added parameters check for ElevatorCoordinatesY class for US270

--- src/domain/Elevator/ElevatorCoordinateY.ts
+++ src/domain/Elevator/ElevatorCoordinateY.ts
@@ -15,12 +15,21 @@ export class ElevatorCoordinateY extends ValueObject<ElevatorCoordinateYProps> {
     return this.props.elevatorCoordinateY
   }
 
-  public static create (elevatorCoordinateY: number): Result<ElevatorCoordinateY> {
+  public static create (elevatorCoordinateY: number, buildingLines: number): Result<ElevatorCoordinateY> {
     const guardResult = Guard.againstNullOrUndefined(elevatorCoordinateY, 'elevatorCoordinateY');
+    const maxPos = buildingLines +1;
     if (!guardResult.succeeded) {
       return Result.fail<ElevatorCoordinateY>(guardResult.message);
-    } else {
-      return Result.ok<ElevatorCoordinateY>(new ElevatorCoordinateY({elevatorCoordinateY : elevatorCoordinateY}))
+    } 
+    
+    if (elevatorCoordinateY <= 0) {
+      return Result.fail<ElevatorCoordinateY>('Elevator Position Y must be a positive number');
     }
+
+    if (elevatorCoordinateY > maxPos) {
+      return Result.fail<ElevatorCoordinateY>('Elevator Position Y must be lower than ' + maxPos);
+    }
+
+      return Result.ok<ElevatorCoordinateY>(new ElevatorCoordinateY({elevatorCoordinateY : elevatorCoordinateY}))
   }
 }
