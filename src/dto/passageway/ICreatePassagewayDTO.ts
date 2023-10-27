5c945acd8100f7e9539a3e85ea471992db29f0b2
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-27 15:36:34 +0100

    Updated Interface CreatePassageway DTO relative to US240

---
+++ src/dto/passageway/ICreatePassagewayDTO.ts
@@ -0,0 +1,9 @@
+export interface ICreatePassagewayDTO {
+    passagewayId: number
+    passagewayCoordinatesTopX: number
+    passagewayCoordinatesTopY: number
+    passagewayCoordinatesBottomX: number
+    passagewayCoordinatesBottomY: number
+    building1ID: number
+    building2ID: number
+}
