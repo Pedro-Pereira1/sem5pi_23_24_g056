792aaa66b2df688149637a2df045f7b08d785894
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-27 20:59:06 +0100

    Updated listBuildingsMaxMinFloorsService relative to US180 changed return to a Promise

--- src/services/building/list/listBuildingsMaxMinFloorsService.ts
+++ src/services/building/list/listBuildingsMaxMinFloorsService.ts
@@ -6,6 +6,7 @@ import IBuildingRepo from "../../IRepos/building/IBuildingRepo";
 import config from "../../../../config";
 import { Building } from "../../../domain/Building/Building";
 import { BuildingMap } from "../../../mappers/building/BuildingMap";
+import { IBuildingListMaxMinDTO } from "../../../dto/building/IBuildingListMaxMinDTO";
 
 @Service()
 export default class listBuildingsMaxMinFloorsService implements IListBuildingsMaxMinFloorsService {
@@ -15,13 +16,19 @@ export default class listBuildingsMaxMinFloorsService implements IListBuildingsM
     )
     {}
 
-    public async listBuildingsMaxMinFloors(max:number, min:number): Promise<Result<IBuildingDTO[]>> {
+    public async listBuildingsMaxMinFloors(max: number, min: number): Promise<Result<IBuildingDTO[]>> {
         let values: IBuildingDTO[]  = [];
+
         const buildings = await this.buildingRepo.findBuildingsMaxMinFloors(max,min)
 
         buildings.forEach((element) => {
             values.push(BuildingMap.toDto(element))
         })
+
+        if(buildings.length === 0) {
+            return Result.fail<IBuildingDTO[]>("null")
+        }
+
         
         return Result.ok<IBuildingDTO[]>(values)
     }
