2ecd0d7ad8439548d164a04857506df12fb06216
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-27 15:36:00 +0100

    Updated Interface Passageway Service relative to US240 added passageway to floorMap

--- src/services/IServices/passageway/create/ICreatePassagewayService.ts
+++ src/services/IServices/passageway/create/ICreatePassagewayService.ts
@@ -1,4 +1,5 @@
 import { Result } from "../../../../core/logic/Result";
+import { IFloorDTO } from "../../../../dto/floor/IFloorDTO";
 import { IPassagewayDTO } from "../../../../dto/passageway/IPassagewayDTO";
 
 export default interface ICreatePassagewayService {
