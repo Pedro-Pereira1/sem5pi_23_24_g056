6be2ea387c1f57d3d1f27f2fa64cc0b697c91422
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-27 20:54:24 +0100

    Updated Floor Route relative to US190/210 added post and get

--- src/api/routes/floorRoute.ts
+++ src/api/routes/floorRoute.ts
@@ -5,6 +5,7 @@ import { Container } from 'typedi';
 import ICreateFloorController from '../../controllers/IControllers/floor/create/ICreateFloorController';
 
 import config from "../../../config";
+import IListAllFloorsController from '../../controllers/IControllers/floor/list/IListAllFloorsController';
 
 const route = Router();
 
@@ -12,13 +13,25 @@ export default (app: Router) => {
     app.use('/floors', route)
 
     const ctrl = Container.get(config.controllers.createFloor.name) as ICreateFloorController
-    
-    //route.post('/createFloor', 
-    //celebrate({
-        //body: Joi.object({
-            //floorNumber:  Joi.number.max(10),
-            //floorDescription: Joi.string().max(255),
-        //}),
-    //}),
-    //(req, res, next) => ctrl.createFloor(req, res, next));
+    const ctrllistAllFloors = Container.get(config.controllers.listAllFloors.name) as IListAllFloorsController
+
+    route.get('/listAllFloors',
+    celebrate({
+        body: Joi.object({
+          buildingID: Joi.string().alphanum().max(5).required(),
+        }),
+    }),
+    (req, res, next) => { ctrllistAllFloors.listAllFloors(req, res, next)
+    });
+
+   route.post('/createFloor',
+    celebrate({
+        body: Joi.object({
+            floorId: Joi.number().required(),
+            floorNumber:  Joi.number().max(10),
+            floorDescription: Joi.string().max(255),
+            buildingCode: Joi.string().alphanum().max(5).required()
+        }),
+    }),
+    (req, res, next) => ctrl.createFloor(req, res, next));
 }
