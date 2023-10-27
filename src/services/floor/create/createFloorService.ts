894cd6564e754260e27d581daf55cf54f5d9af59
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-27 20:51:44 +0100

    Updated create Floor Service relative to US190 with new DTO

--- src/services/floor/create/createFloorService.ts
+++ src/services/floor/create/createFloorService.ts
@@ -8,35 +8,49 @@ import { Floor } from "../../../domain/Floor/Floor";
 import { FloorMaper } from "../../../mappers/floor/FloorMaper";
 import { FloorDescription } from "../../../domain/Floor/FloorDescription";
 import { FloorMap } from "../../../domain/Floor/FloorMap";
+import IBuildingRepo from "../../IRepos/building/IBuildingRepo";
+import { IBuildingDTO } from "../../../dto/building/IBuildingDTO";
+import { BuildingMap } from "../../../mappers/building/BuildingMap";
+import { ICreateFloorDTO } from "../../../dto/floor/ICreateFloorDTO";
+import BuildingCode from "../../../domain/Building/BuildingCode";
 
 @Service()
 export default class CreateFloorService implements ICreateFloorService {
 
     constructor(
-        @Inject(config.repos.floor.name) private floorRepo: IFloorRepo
+        @Inject(config.repos.floor.name) private floorRepo: IFloorRepo,
+        @Inject(config.repos.building.name) private buildingRepo: IBuildingRepo
     ) { }
 
-    public async createFloor(floorDto: IFloorDTO): Promise<Result<IFloorDTO>> {
+    public async createFloor(createFloorDTO: ICreateFloorDTO): Promise<Result<IFloorDTO>> {
+        
         try {
             const FloorOrError = await Floor.create(
                 {
-                    floorDescription: new FloorDescription({ value: floorDto.floorDescription }),
-                    floorNumber: floorDto.floorNumber,
+                    floorDescription: new FloorDescription({ value: createFloorDTO.floorDescription }),
+                    floorNumber: createFloorDTO.floorNumber,
                     floormap: new FloorMap({
                         map: [],
                         passageways: [],
                         elevators: [],
                         rooms: []
                     })
-                }, floorDto.floorId)
+                }, createFloorDTO.floorId)
+
 
             if (FloorOrError.isFailure) {
                 return Result.fail<IFloorDTO>(FloorOrError.errorValue())
             }
 
+            
             const floorResult = FloorOrError.getValue()
+            const buildingResult = await this.buildingRepo.findByBuidingCode(new BuildingCode(createFloorDTO.buildingCode))
+            
+
+            buildingResult.addFloor(floorResult);
 
             await this.floorRepo.save(floorResult);
+            await this.buildingRepo.save(buildingResult);
 
             const floorDtoResult = FloorMaper.toDto(floorResult) as IFloorDTO
             return Result.ok<IFloorDTO>(floorDtoResult)
