117213bfcefa29d75bdbbce695e94c99d247901d
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-29 14:14:51 +0000

    Updated Floor Route relative to US200 added route to edit Floor

--- src/api/routes/floorRoute.ts
+++ src/api/routes/floorRoute.ts
@@ -6,6 +6,7 @@ import ICreateFloorController from '../../controllers/IControllers/floor/create/
 
 import config from "../../../config";
 import IListAllFloorsController from '../../controllers/IControllers/floor/list/IListAllFloorsController';
+import IEditFloorController from '../../controllers/IControllers/floor/edit/IEditFloorController';
 
 const route = Router();
 
@@ -14,6 +15,7 @@ export default (app: Router) => {
 
     const ctrl = Container.get(config.controllers.createFloor.name) as ICreateFloorController
     const ctrllistAllFloors = Container.get(config.controllers.listAllFloors.name) as IListAllFloorsController
+    const ctrlEditFloor = Container.get(config.controllers.editFloor.name) as IEditFloorController
 
     route.get('/listAllFloors/:buildingId',
     (req, res, next) => { ctrllistAllFloors.listAllFloors(req, res, next);
@@ -30,4 +32,15 @@ export default (app: Router) => {
         }),
     }),
     (req, res, next) => ctrl.createFloor(req, res, next));
+
+    route.put('/editFloor',
+		celebrate({
+            body: Joi.object({
+                floorId: Joi.number().required(),
+                floorNumber:  Joi.number().max(10),
+                floorDescription: Joi.string().max(255),
+            }),
+		}),
+		(req, res, next) => ctrlEditFloor.editFloor(req, res, next)
+	);
 }
