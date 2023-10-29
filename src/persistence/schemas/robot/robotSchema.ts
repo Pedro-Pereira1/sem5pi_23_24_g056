d10cf270741b8ede0c1dab6a916b9bcc45d960ce
Pedro Pereira <1211131@isep.ipp.pt>
2023-10-29 22:41:23 +0000

    Added Robot schema relative to US360

---
+++ src/persistence/schemas/robot/robotSchema.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import IRobotPersistence from "../../../dataschema/robot/IRobotPersistence";
+
+
+const robotSchema = new mongoose.Schema(
+    {
+        code: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        nickname: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        serialNumber: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: false
+        }
+        
+    },
+    {
+        timestamps: true
+    }
+)
+
+export default mongoose.model<IRobotPersistence & mongoose.Document>('Robot', robotSchema)
