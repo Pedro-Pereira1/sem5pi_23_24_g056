cf1fef2b930de2d79f4f5c434a30a87a013e3f5c
Tiago <1211128@isep.ipp.pt>
2023-11-01 17:15:39 +0000

    Added methods that need to be implemented, regarding US310

--- src/services/IRepos/room/IRoomRepo.ts
+++ src/services/IRepos/room/IRoomRepo.ts
@@ -2,5 +2,7 @@ import { Repo } from "../../../core/infra/Repo";
 import { Room } from "../../../domain/Room/Room";
 
 export default interface IRoomRepo extends Repo<Room> {
-    findById(id: number): Promise<Room>
+    findById(id: string): Promise<Room>
+    save(room: Room): Promise<Room>
+    exists(room: Room)
 }
