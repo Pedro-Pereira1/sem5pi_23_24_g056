286c97861cb222e357cdffaf89563422437bd9cc
Tiago <1211128@isep.ipp.pt>
2023-11-01 15:52:45 +0000

    Removed unused parameters, regarding US270

--- src/services/elevator/create/createElevatorService.ts
+++ src/services/elevator/create/createElevatorService.ts
@@ -6,44 +6,58 @@ import { Elevator } from "../../../domain/Elevator/Elevator";
 import ElevatorMap from "../../../mappers/elevator/ElevatorMap";
 import ICreateElevatorService from "../../IServices/elevator/ICreateElevatorService";
 import IElevatorDTO from "../../../dto/elevator/IElevatorDTO";
-import { ElevatorCoordinateX } from "../../../domain/Elevator/ElevatorCoordinateX";
-import { ElevatorCoordinateY } from "../../../domain/Elevator/ElevatorCoordinateY";
 import { ElevatorIdentificationNumber } from "../../../domain/Elevator/ElevatorIdentificationNumber";
 import { ElevatorBrand } from "../../../domain/Elevator/ElevatorBrand";
 import { ElevatorSerialNumber } from "../../../domain/Elevator/ElevatorSerialNumber";
 import { ElevatorModel } from "../../../domain/Elevator/ElevatorModel";
 import { ElevatorDescription } from "../../../domain/Elevator/ElevatorDescription";
-
+import IBuildingRepo from "../../IRepos/building/IBuildingRepo";
+import ICreateElevatorDTO from "../../../dto/elevator/ICreateElevatorDTO";
+import BuildingCode from "../../../domain/Building/BuildingCode";
+import IFloorRepo from "../../IRepos/floor/IFloorRepo";
+import { UniqueEntityID } from "../../../core/domain/UniqueEntityID";
 
 @Service()
 export default class CreateElevatorService implements ICreateElevatorService {
 
     constructor(
-        @Inject(config.repos.elevator.name) private elevatorRepo: IElevatorRepo
+        @Inject(config.repos.elevator.name) private elevatorRepo: IElevatorRepo,
+        @Inject(config.repos.building.name) private buildingRepo: IBuildingRepo,
+        @Inject(config.repos.floor.name) private floorRepo: IFloorRepo
     ) { }
 
-    public async createElevator(elevatorDto: IElevatorDTO): Promise<Result<IElevatorDTO>> {
+    public async createElevator(elevatorDto: ICreateElevatorDTO): Promise<Result<IElevatorDTO>> {
         try{
-            const ElevatorOrError = await Elevator.create(
+            const building = await this.buildingRepo.findByBuidingCode(new BuildingCode(elevatorDto.buildingCode))
+            if (!building) throw new Error("Building does not exist!")
+
+            const floor = await this.floorRepo.findById(elevatorDto.floorId)
+            if (floor === null) throw new Error("Floor does not exist!")
+
+            //console.log(floor.props.floormap.elevatorsId)
+
+            const elevatorOrError = await Elevator.create(
                 {
-                    elevatorCoordinateX: new ElevatorCoordinateX({elevatorCoordinateX: elevatorDto.elevatorCoordinateX}),
-                    elevatorCoordinateY: new ElevatorCoordinateY({elevatorCoordinateY: elevatorDto.elevatorCoordinateY}),
-                    elevatorIdentificationNumber: new ElevatorIdentificationNumber({identificationNumber: elevatorDto.elevatorIdentificationNumber}),
-                    elevatorBrand: new ElevatorBrand({brand: elevatorDto.elevatorBrand}),
-                    elevatorDescription: new ElevatorDescription({description: elevatorDto.elevatorDescription}),
-                    elevatorModel: new ElevatorModel({model: elevatorDto.elevatorModel}),
-                    elevatorSerialNumber: new ElevatorSerialNumber({serialNumber: elevatorDto.elevatorSerialNumber})
-                })
-
-            if (ElevatorOrError.isFailure) {
-                return Result.fail<IElevatorDTO>(ElevatorOrError.errorValue())
+                    elevatorIdentificationNumber: ElevatorIdentificationNumber.create(elevatorDto.elevatorIdentificationNumber).getValue(),
+                    elevatorBrand: ElevatorBrand.create(elevatorDto.elevatorBrand).getValue(),
+                    elevatorDescription: ElevatorDescription.create(elevatorDto.elevatorDescription).getValue(),
+                    elevatorModel: ElevatorModel.create(elevatorDto.elevatorModel).getValue(),
+                    elevatorSerialNumber: ElevatorSerialNumber.create(elevatorDto.elevatorSerialNumber).getValue()
+                }, new UniqueEntityID (1))
+                
+            
+            if (elevatorOrError.isFailure) {
+                return Result.fail<IElevatorDTO>(elevatorOrError.errorValue())
             }
-
-            const elevatorResult = ElevatorOrError.getValue()
-
+            
+            const elevatorResult = elevatorOrError.getValue();
+            
+            floor.addElevators(elevatorResult)
             await this.elevatorRepo.save(elevatorResult);
 
+            await this.floorRepo.save(floor);
             const ElevatorDtoResult = ElevatorMap.toDto(elevatorResult) as IElevatorDTO
+
             return Result.ok<IElevatorDTO>(ElevatorDtoResult)
 
         } catch(e) {
