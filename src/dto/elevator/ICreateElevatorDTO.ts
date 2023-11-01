5b91dc8894930fd3739554a6d676e261c8f9e0e5
Tiago <1211128@isep.ipp.pt>
2023-11-01 15:50:37 +0000

    Added new DTO class to pass parameters when creating an elevator, such as building code and floor id, regarding US270

---
+++ src/dto/elevator/ICreateElevatorDTO.ts
@@ -0,0 +1,9 @@
+export default interface ICreateElevatorDTO {
+    elevatorIdentificationNumber: number
+    elevatorBrand: string
+    elevatorDescription: string
+    elevatorModel: string
+    elevatorSerialNumber: string
+    buildingCode: string
+    floorId: number
+}
