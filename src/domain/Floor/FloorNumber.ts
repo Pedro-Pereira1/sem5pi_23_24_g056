84f4b02da02ec4e874d9ad6fb898ea77acb4ac6e
jose <1211089@isep.ipp.pt>
2023-11-01 17:39:58 +0000

    Added method number

--- src/domain/Floor/FloorNumber.ts
+++ src/domain/Floor/FloorNumber.ts
@@ -1,13 +1,17 @@
 import { ValueObject } from "../../core/domain/ValueObject";
 
 interface IFloorNumberProps {
-    number: number
+  number: number
 }
 
 export default class FloorNumber extends ValueObject<IFloorNumberProps> {
 
   constructor(floorNumberProps: IFloorNumberProps) {
     super(floorNumberProps)
+  }
+
+  get number(): number {
+    return this.props.number
   }
 
 
