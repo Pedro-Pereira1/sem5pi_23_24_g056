64d08251df5b81d93cc7a3e63d796b25cfa0c868
Tiago <1211128@isep.ipp.pt>
2023-11-02 23:30:19 +0000

    Added route to handle edit elevators requests, regarding US280

--- src/api/routes/elevatorRoute.ts
+++ src/api/routes/elevatorRoute.ts
@@ -5,6 +5,7 @@ import { Container } from 'typedi';
 
 import config from "../../../config";
 import ICreateElevatorController from '../../controllers/IControllers/elevator/create/ICreateElevatorController';
+import IEditElevatorController from '../../controllers/IControllers/elevator/edit/IEditElevatorController';
 
 const route = Router();
 
@@ -12,6 +13,7 @@ export default (app: Router) => {
     app.use('/elevators', route)
 
     const ctrl = Container.get(config.controllers.createElevator.name) as ICreateElevatorController
+    const ctrlEdit = Container.get(config.controllers.editElevator.name) as IEditElevatorController
     
     route.post('/createElevator', 
     celebrate({
@@ -26,4 +28,19 @@ export default (app: Router) => {
         }),
     }),
     (req, res, next) => ctrl.createElevator(req, res, next));
+
+    route.put('/editElevator', 
+    celebrate({
+        body: Joi.object({
+            elevatorIdentificationNumber: Joi.number().required().min(1),
+            elevatorBrand: Joi.string(),
+            elevatorDescription: Joi.string(),
+            elevatorModel: Joi.string(),
+            elevatorSerialNumber: Joi.string(),
+            buildingCode: Joi.string().required(),
+            floorsIdToAdd: Joi.array().items(Joi.number()),
+            floorsIdToRemove: Joi.array().items(Joi.number())
+        }),
+    }),
+    (req, res, next) => ctrlEdit.editElevator(req, res, next));
 }
