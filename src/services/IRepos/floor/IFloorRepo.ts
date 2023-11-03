34d24f55b17ca10b29dfa426f8576a7dfbb21405
Tiago <1211128@isep.ipp.pt>
2023-11-03 15:03:06 +0000

    Added method to find all floors that an elevator reaches, regarding US280

--- src/services/IRepos/floor/IFloorRepo.ts
+++ src/services/IRepos/floor/IFloorRepo.ts
@@ -4,4 +4,5 @@ import { Floor } from "../../../domain/Floor/Floor";
 export default interface IFloorRepo extends Repo<Floor> {
     save(floor: Floor): Promise<Floor>
     findById(id: number): Promise<Floor>
+    findByElevator(id: number): Promise<Floor[]>
 }
