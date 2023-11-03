0bdef4e618648d3291297ebf9a42cbfc9df561f0
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-03 14:15:25 +0000

    Update class passagewaySchema relative US240 on removed coordinates

--- src/persistence/schemas/passageway/passagewaySchema.ts
+++ src/persistence/schemas/passageway/passagewaySchema.ts
@@ -4,14 +4,7 @@ import IPassagewayPersistence from '../../../dataschema/passageway/IPassagewayPe
 const PassagewaySchema = new mongoose.Schema(
     {
         passagewayId: { type: Number, unique: true },
-        passagewayCoordinatesTopX: { type: Number },
-        passagewayCoordinatesTopY: { type: Number },
-        passagewayCoordinatesBottomX: { type: Number },
-        passagewayCoordinatesBottomY: {type: Number },
-        passagewayCoordinatesTopXB2: { type: Number },
-        passagewayCoordinatesTopYB2: { type: Number },
-        passagewayCoordinatesBottomXB2: { type: Number },
-        passagewayCoordinatesBottomYB2: {type: Number },
+
     },
     {
         timestamps: true
