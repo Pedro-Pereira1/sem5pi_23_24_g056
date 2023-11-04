52d98041fa52664ba58b45414b422e27e7262d78
jose <1211089@isep.ipp.pt>
2023-11-04 00:12:07 +0000

    US 370 created InhibitRobotController

---
+++ src/controllers/robot/inhibit/inhibitRobotController.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import IInhibitRobotController from "../../IControllers/robot/inhibit/IInhibitRobotController";
+import { Inject, Service } from "typedi";
+import config from "../../../../config";
+import IInhibitRobotService from "../../../services/IServices/robot/inhibit/IIhibitRobotService";
+import IInhibitRobotDTO from "../../../dto/robot/IInhibitRobotDTO";
+
+@Service()
+export default class InhibitRobotController implements IInhibitRobotController {
+    
+    constructor(
+        @Inject(config.services.inhibitRobot.name) private inhibitRobotService: IInhibitRobotService
+    ){}
+
+    public async inhibitRobot(req: Request, res: Response, next: NextFunction) {
+        const robotDto = await this.inhibitRobotService.inhibitRobot(req.body as IInhibitRobotDTO)
+
+        if (robotDto.isFailure) {
+            return res.status(400).send(robotDto.errorValue())
+        }
+
+        return res.status(201).send(robotDto.getValue())
+    }
+
+}
