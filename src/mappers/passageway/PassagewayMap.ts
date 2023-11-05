16adc55e9cfc45d436a2a98c89447e0240e85e38
Tiago <1211128@isep.ipp.pt>
2023-11-05 00:01:25 +0000

    Added method to pass response data to dto, for US260

--- src/mappers/passageway/PassagewayMap.ts
+++ src/mappers/passageway/PassagewayMap.ts
@@ -3,6 +3,8 @@ import { Mapper } from "../../core/infra/Mapper";
 import IPassagewayPersistence from "../../dataschema/passageway/IPassagewayPersistence";
 import { Passageway } from "../../domain/Passageway/Passageway";
 import { IPassagewayDTO } from "../../dto/passageway/IPassagewayDTO";
+import { NumericDictionary } from "lodash";
+import { IListPassagewaysBetween2BuildingsDTO } from "../../dto/passageway/IListPassagewaysBetween2BuildingsDTO";
 
 export class PassagewayMap extends Mapper<Passageway> {
 
@@ -10,6 +12,14 @@ export class PassagewayMap extends Mapper<Passageway> {
         return {
             passagewayId: Number(passageway.id.toValue())
         } as IPassagewayDTO
+    }
+
+    public static toDtoList(passageway: Passageway, floorNumberBuilding1: number, floorNumberBuilding2: number): IListPassagewaysBetween2BuildingsDTO {
+        return {
+            passagewayId: Number(passageway.id.toValue()),
+            floorNumberBuilding1: floorNumberBuilding1,
+            floorNumberBuilding2: floorNumberBuilding2
+        } as IListPassagewaysBetween2BuildingsDTO
     }
 
     public static toDomain(IPassagewayDTO: any | Model<IPassagewayPersistence & Document>): Passageway {
