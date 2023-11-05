f257ef8868e135fd3b7a83d40b1e9c3bb75c1962
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-05 11:50:22 +0000

    Updated class IEditFloorService.ts relative to US200, updated argument to an IEditFloorDTO

--- src/services/IServices/floor/edit/IEditFloorService.ts
+++ src/services/IServices/floor/edit/IEditFloorService.ts
@@ -1,6 +1,7 @@
 import { Result } from "../../../../core/logic/Result";
 import { IFloorDTO } from "../../../../dto/floor/IFloorDTO";
+import { IEditFloorDTO } from "../../../../dto/floor/IEditFloorDTO";
 
 export default interface ICreateFloorService {
-    editFloor(FloorDTO: IFloorDTO): Promise<Result<IFloorDTO>>
+    editFloor(FloorDTO: IEditFloorDTO): Promise<Result<IFloorDTO>>
 }
