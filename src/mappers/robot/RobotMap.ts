9965794fb0dc5fba9e86f0a214fc58ae80365ae5
Tiago <1211128@isep.ipp.pt>
2023-11-05 01:20:24 +0000

    Fixed toDomain method, it was not getting the robotType

--- src/mappers/robot/RobotMap.ts
+++ src/mappers/robot/RobotMap.ts
@@ -11,6 +11,7 @@ import { SerialNumber } from "../../domain/Robot/SerialNumber";
 import { RobotType } from "../../domain/RobotType/RobotType";
 import { RobotDescription } from "../../domain/Robot/RobotDescription";
 import { Code } from "../../domain/Robot/Code";
+import IRobotTypeRepo from "../../services/IRepos/robotType/IRobotTypeRepo";
 
 
 export class RobotMap extends Mapper<Robot> {
@@ -27,8 +28,10 @@ export class RobotMap extends Mapper<Robot> {
     }
 
     public static async toDomain(robotDTO: any | Model<IRobotPersistence & Document>): Promise<Robot> {
+        const robotTypeRepo: IRobotTypeRepo = Container.get(config.repos.robotType.name)
 
-        const RobotOrError = Robot.create(robotDTO, robotDTO.type, robotDTO.code)
+        const type = await robotTypeRepo.findById(robotDTO.type)
+        const RobotOrError = Robot.create(robotDTO, type, robotDTO.code)
 
         return RobotOrError.isSuccess ? RobotOrError.getValue() : null
     }
