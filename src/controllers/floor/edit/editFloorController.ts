55a33acceeb7c024e355a5921aa2480934b7552c
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-05 11:32:20 +0000

    Updated class editFloorController.ts relative to US200, updated response order

--- src/controllers/floor/edit/editFloorController.ts
+++ src/controllers/floor/edit/editFloorController.ts
@@ -3,23 +3,22 @@ import IEditFloorController from '../../IControllers/floor/edit/IEditFloorContro
 import { Inject, Service } from 'typedi';
 import config from "../../../../config";
 import { Result } from '../../../core/logic/Result';
-import { IFloorDTO } from '../../../dto/floor/IFloorDTO';
+import { IEditFloorDTO } from '../../../dto/floor/IEditFloorDTO';
 import IEditFloorService from '../../../services/IServices/floor/edit/IEditFloorService';
-
-
+import { IFloorDTO } from '../../../dto/floor/IFloorDTO';
 @Service()
 export default class EditFloorController implements IEditFloorController {
 	constructor(
 		@Inject(config.services.editFloor.name) private service: IEditFloorService
-	) 
+	)
     {}
 
 	public async editFloor(req: Request, res: Response, next: NextFunction) {
 		try {
-			const floorOrError = await this.service.editFloor( req.body as IFloorDTO) as Result<IFloorDTO>
+			const floorOrError = await this.service.editFloor(req.body as IEditFloorDTO) as Result<IFloorDTO>
 
 			if (floorOrError.isFailure) {
-				return res.status(402).json(floorOrError.error);
+				return res.status(400).send(floorOrError.error);
 			}
 			const FloorDTO = floorOrError.getValue();
 			return res.status(200).json(FloorDTO);
