ac4b608c14922b229394c5699c5104e0fdcf86e3
Tiago <1211128@isep.ipp.pt>
2023-11-05 00:10:38 +0000

    Added findAll method to robot repo interface, regarding US380

--- src/services/IRepos/robot/IRobotRepo.ts
+++ src/services/IRepos/robot/IRobotRepo.ts
@@ -1,8 +1,10 @@
 import { Repo } from "../../../core/infra/Repo";
 import { Robot } from "../../../domain/Robot/Robot";
+import { IRobotDTO } from "../../../dto/robot/IRobotDTO";
 
 export default interface IRobotRepo extends Repo<Robot> {
     findBySerialNumberAndType(serialNumber: string, type: string): unknown;
     save(robot: Robot): Promise<Robot>
     findById(id: string): Promise<Robot>
+    findAll(): Promise<Robot[]>
 }
