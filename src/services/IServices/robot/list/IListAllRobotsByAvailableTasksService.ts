32d5d7a1deab8e91b100288e58b87bdeb394367d
jose <1211089@isep.ipp.pt>
2023-11-05 11:20:25 +0000

    US 390 Changed return type of services

--- src/services/IServices/robot/list/IListAllRobotsByAvailableTasksService.ts
+++ src/services/IServices/robot/list/IListAllRobotsByAvailableTasksService.ts
@@ -1,7 +1,8 @@
 import { Result } from "../../../../core/logic/Result";
 import { Robot } from "../../../../domain/Robot/Robot";
 import IListRobotsByAvailableTasksDTO from "../../../../dto/robot/IListRobotsByAvailableTasksDTO";
+import { IRobotDTO } from "../../../../dto/robot/IRobotDTO";
 
 export default interface IListRobotsByAvailableTasksService {
-    listRobotsByAvailableTasks(tasksDto: IListRobotsByAvailableTasksDTO): Promise<Result<Robot>>
+    listRobotsByAvailableTasks(tasksDto: IListRobotsByAvailableTasksDTO): Promise<Result<IRobotDTO[]>>
 }
--- src/services/IServices/robot/list/IListRobotsByDesignationService.ts
+++ src/services/IServices/robot/list/IListRobotsByDesignationService.ts
@@ -1,6 +1,7 @@
 import { Result } from "../../../../core/logic/Result";
 import { Robot } from "../../../../domain/Robot/Robot";
+import { IRobotDTO } from "../../../../dto/robot/IRobotDTO";
 
 export default interface IListRobotsByDesignationService {
-    listRobotsByDesignation(nickname: string): Promise<Result<Robot>>
+    listRobotsByDesignation(nickname: string): Promise<Result<IRobotDTO[]>>
 }
