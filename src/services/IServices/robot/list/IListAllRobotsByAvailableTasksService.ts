d3fe79e1b6ce018ff753eb8b059094e559cebcff
jose <1211089@isep.ipp.pt>
2023-11-05 11:04:05 +0000

    US 390 changed names for services interfaces

--- src/services/IServices/robot/list/IListAllRobotsByAvailableTasksService.ts
+++ src/services/IServices/robot/list/IListAllRobotsByAvailableTasksService.ts
@@ -2,6 +2,6 @@ import { Result } from "../../../../core/logic/Result";
 import { Robot } from "../../../../domain/Robot/Robot";
 import IListRobotsByAvailableTasksDTO from "../../../../dto/robot/IListRobotsByAvailableTasksDTO";
 
-export default interface IListRobotsByAvailableTasks {
+export default interface IListRobotsByAvailableTasksService {
     listRobotsByAvailableTasks(tasksDto: IListRobotsByAvailableTasksDTO): Promise<Result<Robot>>
 }
--- src/services/IServices/robot/list/IListRobotsByDesignationService.ts
+++ src/services/IServices/robot/list/IListRobotsByDesignationService.ts
@@ -1,6 +1,6 @@
 import { Result } from "../../../../core/logic/Result";
 import { Robot } from "../../../../domain/Robot/Robot";
 
-export default interface IListRobotsByDesignation {
+export default interface IListRobotsByDesignationService {
     listRobotsByDesignation(nickname: string): Promise<Result<Robot>>
 }
