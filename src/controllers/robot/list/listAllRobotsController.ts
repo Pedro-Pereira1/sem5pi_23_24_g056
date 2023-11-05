7f5fcc5e05817ba52db305eb004ce1a3ab718019
Tiago <1211128@isep.ipp.pt>
2023-11-05 01:19:22 +0000

    Fixed implemented interface, regarding US380

--- src/controllers/robot/list/listAllRobotsController.ts
+++ src/controllers/robot/list/listAllRobotsController.ts
@@ -1,18 +1,18 @@
 import { Inject, Service } from "typedi";
-import IListAllBuildingsController from "../../IControllers/building/list/IListAllBUildingsController";
 import { Request, Response, NextFunction } from "express";
 import config from "../../../../config";
 import IListAllRobotsService from "../../../services/IServices/robot/list/IListAllRobotsService";
+import IListAllRobotsController from "../../IControllers/robot/list/IListAllRobotsController";
 
 @Service()
-export default class ListAllRobotsController implements IListAllBuildingsController {
+export default class ListAllRobotsController implements IListAllRobotsController {
 
     constructor(
         @Inject(config.services.listAllRobots.name) private listAllRobotsService: IListAllRobotsService
     )
     {}
 
-    public async listAllBuildings(req: Request, res: Response, next: NextFunction) {
+    public async listAllRobots(req: Request, res: Response, next: NextFunction) {
         try {
             const robots = await this.listAllRobotsService.listAllRobots()
 
