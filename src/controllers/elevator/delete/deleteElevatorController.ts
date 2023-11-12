a4e5a77a931d73576a88b8fea662d5e206e72158
Alexandre Geração <1211151@isep.ipp.pt>
2023-11-12 18:55:29 +0000

    Added deleteElevatorController to delete Elevators

---
+++ src/controllers/elevator/delete/deleteElevatorController.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import { Inject, Service } from "typedi";
+import config from "../../../../config";
+import { Result } from "../../../core/logic/Result";
+import IDeleteElevatorController from "../../IControllers/elevator/delete/IDeleteElevatorController";
+import IDeleteElevatorService from "../../../services/IServices/elevator/delete/IDeleteElevatorService";
+import IEditElevatorDTO from "../../../dto/elevator/IEditElevatorDTO";
+
+@Service()
+export default class DeleteElevatorController implements IDeleteElevatorController {
+
+    constructor(
+        @Inject(config.services.deleteElevator.name) private service: IDeleteElevatorService
+    )
+    {}
+
+    public async deleteElevator(req: Request, res: Response, next: NextFunction) {
+        try {
+            const elevatorOrError = await this.service.deleteElevator(req.body as IEditElevatorDTO) as Result<boolean>
+
+            if (elevatorOrError.isFailure) {
+                return res.status(400).send(elevatorOrError.errorValue())
+            }
+
+            return res.status(201).json("Elevator deleted successfully");
+
+        }catch (e){
+            return next(e);
+        }
+    }
+}
