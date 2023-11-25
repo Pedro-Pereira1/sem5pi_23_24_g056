fd63c699fb23540c384dbeb92421228111d2f388
Alexandre Geração <1211151@isep.ipp.pt>
2023-11-25 23:16:24 +0000

    Created IListAllElevatorsController to list all the elevators

---
+++ src/controllers/IControllers/elevator/list/IListAllElevatorsController.ts
@@ -0,0 +1,5 @@
+import { Request, Response, NextFunction } from "express";
+
+export default interface IListAllElevatorsController {
+    listAllElevators(req: Request, res: Response, next: NextFunction)
+}
