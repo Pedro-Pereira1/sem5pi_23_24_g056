1a20e3c4ee7f57d459dc457e4c7992ccc952b47b
Pedro Pereira <1211131@isep.ipp.pt>
2023-12-13 09:24:39 +0000

    SC-14 SC-59 Add findFloorsByPassageway method to IListPassagewaysBetween2BuildingsService

--- src/services/IServices/passageway/list/IListPassagewaysBetween2BuildingsService.ts
+++ src/services/IServices/passageway/list/IListPassagewaysBetween2BuildingsService.ts
@@ -1,6 +1,8 @@
 import { Result } from "../../../../core/logic/Result";
+import { Floor } from "../../../../domain/Floor/Floor";
 import { IListPassagewaysBetween2BuildingsDTO } from "../../../../dto/passageway/IListPassagewaysBetween2BuildingsDTO";
 
 export default interface IListPassagewaysBetween2BuildingsService {
     listPassagewaysBetween2Buildings(building1Code: string, building2Code: string): Promise<Result<IListPassagewaysBetween2BuildingsDTO[]>>
+    findFloorsByPassageway(passagewayId: number): Promise<Result<Floor[]>>
 }
