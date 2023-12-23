4c4aca24f20dbf9ce7a23d8abfe1ab9c2c8a3a56
Pedro Pereira <1211131@isep.ipp.pt>
2023-12-23 19:43:19 +0000

    SC-13 SC-62 Add listFloorsByElevatorId method to IListAllElevatorsService

--- src/services/IServices/elevator/list/IListAllElevatorsService.ts
+++ src/services/IServices/elevator/list/IListAllElevatorsService.ts
@@ -1,6 +1,8 @@
 import {Result} from "../../../../core/logic/Result";
 import IListAllElevatorsDTO from "../../../../dto/elevator/IListAllElevatorsDTO";
+import { IFloorDTO } from "../../../../dto/floor/IFloorDTO";
 
 export default interface IListAllElevatorsService {
     listAllElevators(): Promise<Result<IListAllElevatorsDTO[]>>
+    listFloorsByElevatorId(elevatorId: string): Promise<Result<IFloorDTO[]>>
 }
