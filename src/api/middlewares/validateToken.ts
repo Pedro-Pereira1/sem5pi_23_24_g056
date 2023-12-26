8f566a03d819d30961be1e4628e411d7c55751e9
Jose Gouveia <1211089@isep.ipp.pt>
2023-12-26 20:20:35 +0000

    SC-24 Added tests for US 1310

--- src/api/middlewares/validateToken.ts
+++ src/api/middlewares/validateToken.ts
@@ -4,12 +4,12 @@ var config = require('../../../config')
 function validateToken(req, res, next) {
     var auth = req.headers['authorization']
     if (!auth) {
-        return res.status(401).send({ auth: false, message: 'No token provided.' });
+        return res.status(401)
     }
     var token = auth.split(' ')[1];
     jwt.verify(token, "u2mUMiNxgfIbfXIOhOFskAI8o6doVRCH", function (err, decoded) { 
         if (err) {
-            return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+            return res.status(500)
         }
         req.userId = decoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier']
         req.userRole = decoded['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']
