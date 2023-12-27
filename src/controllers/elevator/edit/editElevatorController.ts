ebabb7046c62cc15c7fa574eb12990dbed359d45
Pedro Pereira <1211131@isep.ipp.pt>
2023-12-27 13:44:07 +0000

    SC-23 Add authorization check in editElevator method

--- src/controllers/elevator/edit/editElevatorController.ts
+++ src/controllers/elevator/edit/editElevatorController.ts
@@ -6,16 +6,23 @@ import { Result } from "../../../core/logic/Result";
 import IEditElevatorController from "../../IControllers/elevator/edit/IEditElevatorController";
 import IEditElevatorService from "../../../services/IServices/elevator/edit/IEditElevatorService";
 import IEditElevatorDTO from "../../../dto/elevator/IEditElevatorDTO";
+import { IAuthService } from "../../../services/IServices/auth/IAuthService";
 
 @Service()
 export default class EditElevatorController implements IEditElevatorController {
 
     constructor(
-        @Inject(config.services.editElevator.name) private service: IEditElevatorService
+        @Inject(config.services.editElevator.name) private service: IEditElevatorService,
+        @Inject(config.services.auth.name) private authService: IAuthService
     )
     {}
 
     public async editElevator(req: Request, res: Response, next: NextFunction) {
+        //@ts-ignore
+        let userRole = req.userRole;
+        if(!this.authService.validatePermission(userRole, ["CampusManager"])){
+            return res.status(401).send("Unauthorized");
+        }
         try {
             const elevatorOrError = await this.service.editElevator(req.body as IEditElevatorDTO) as Result<IElevatorDTO>
 
