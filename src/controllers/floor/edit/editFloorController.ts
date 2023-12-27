dff5cd5b1512fb5494da5ae8aeefbad2e365b26e
Pedro Pereira <1211131@isep.ipp.pt>
2023-12-27 13:44:42 +0000

    SC-23 Add authentication check in EditFloorController

--- src/controllers/floor/edit/editFloorController.ts
+++ src/controllers/floor/edit/editFloorController.ts
@@ -6,14 +6,21 @@ import { Result } from '../../../core/logic/Result';
 import { IEditFloorDTO } from '../../../dto/floor/IEditFloorDTO';
 import IEditFloorService from '../../../services/IServices/floor/edit/IEditFloorService';
 import { IFloorDTO } from '../../../dto/floor/IFloorDTO';
+import { IAuthService } from '../../../services/IServices/auth/IAuthService';
 @Service()
 export default class EditFloorController implements IEditFloorController {
 	constructor(
-		@Inject(config.services.editFloor.name) private service: IEditFloorService
+		@Inject(config.services.editFloor.name) private service: IEditFloorService,
+        @Inject(config.services.auth.name) private authService: IAuthService
 	)
     {}
 
 	public async editFloor(req: Request, res: Response, next: NextFunction) {
+		//@ts-ignore
+        let userRole = req.userRole;
+        if(!this.authService.validatePermission(userRole, ["CampusManager"])){
+            return res.status(401).send("Unauthorized");
+        }
 		try {
 			const floorOrError = await this.service.editFloor(req.body as IEditFloorDTO) as Result<IFloorDTO>
 
