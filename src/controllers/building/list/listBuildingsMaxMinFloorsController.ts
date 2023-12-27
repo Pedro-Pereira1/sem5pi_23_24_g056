7eb2d7b995ae3ec0ce0adda7c2efad3d46e731f7
Pedro Pereira <1211131@isep.ipp.pt>
2023-12-27 13:30:10 +0000

    SC-23 Add authorization check in listBuildingsMaxMinFloorsController

--- src/controllers/building/list/listBuildingsMaxMinFloorsController.ts
+++ src/controllers/building/list/listBuildingsMaxMinFloorsController.ts
@@ -7,16 +7,23 @@ import IListBuildingsMaxMinFloorsService from "../../../services/IServices/build
 import { IBuildingListMaxMinDTO } from "../../../dto/building/IBuildingListMaxMinDTO";
 import { Result } from "../../../core/logic/Result";
 import { IBuildingDTO } from "../../../dto/building/IBuildingDTO";
+import { IAuthService } from "../../../services/IServices/auth/IAuthService";
 
 @Service()
 export default class ListBuildingsMaxMinFloorsController implements IListBuildingsMaxMinFloorsController {
 
     constructor(
-        @Inject(config.services.listBuildingsMaxMinFloors.name) private listBuildingsMaxMinFloorsService: IListBuildingsMaxMinFloorsService
+        @Inject(config.services.listBuildingsMaxMinFloors.name) private listBuildingsMaxMinFloorsService: IListBuildingsMaxMinFloorsService,
+        @Inject(config.services.auth.name) private authService: IAuthService
     )
     {}
 
     public async listBuildingsMaxMinFloors(req: Request, res: Response, next: NextFunction) {
+        //@ts-ignore
+        let userRole = req.userRole;
+        if(!this.authService.validatePermission(userRole, ["CampusManager"])){
+            return res.status(401).send("Unauthorized");
+        }
         try{
             const max = Number(req.params.max);
             const min = Number(req.params.min);
