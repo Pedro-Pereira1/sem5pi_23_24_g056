88801bad43911b201ffbb366a860ff1e1ef40b8a
Pedro Pereira <1211131@isep.ipp.pt>
2023-12-27 13:30:25 +0000

    SC-23 Add authentication to inhibitRobotController

--- src/controllers/robot/inhibit/inhibitRobotController.ts
+++ src/controllers/robot/inhibit/inhibitRobotController.ts
@@ -4,15 +4,23 @@ import { Inject, Service } from "typedi";
 import config from "../../../../config";
 import IInhibitRobotService from "../../../services/IServices/robot/inhibit/IIhibitRobotService";
 import IInhibitRobotDTO from "../../../dto/robot/IInhibitRobotDTO";
+import { IAuthService } from "../../../services/IServices/auth/IAuthService";
 
 @Service()
 export default class InhibitRobotController implements IInhibitRobotController {
     
     constructor(
-        @Inject(config.services.inhibitRobot.name) private inhibitRobotService: IInhibitRobotService
+        @Inject(config.services.inhibitRobot.name) private inhibitRobotService: IInhibitRobotService,
+        @Inject(config.services.auth.name) private authService: IAuthService
     ){}
 
     public async inhibitRobot(req: Request, res: Response, next: NextFunction) {
+        //@ts-ignore
+        let userRole = req.userRole;
+        if(!this.authService.validatePermission(userRole, ["FleetManager"])){
+            return res.status(401).send("Unauthorized");
+        }
+        
         const robotDto = await this.inhibitRobotService.inhibitRobot(req.body as IInhibitRobotDTO)
 
         if (robotDto.isFailure) {
