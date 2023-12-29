62150c2fa79d4aee35b56736d9fe4efae5d4afc0
Pedro Pereira <1211131@isep.ipp.pt>
2023-12-29 21:59:06 +0000

    SC-48 Add listAllRooms method to IListAllRoomsInBuildingController and IListAllRoomsService

--- src/controllers/IControllers/room/list/IListAllRoomsInBuildingController.ts
+++ src/controllers/IControllers/room/list/IListAllRoomsInBuildingController.ts
@@ -2,4 +2,5 @@ import { NextFunction, Request, Response } from "express";
 
 export default interface IListAllRoomsInBuildingController{
     listAllRoomsInBuilding(req: Request, res: Response, next: NextFunction)
+    listAllRooms(req: Request, res: Response, next: NextFunction)
 }
